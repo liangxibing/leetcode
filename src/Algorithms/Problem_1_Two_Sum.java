f009395220044dcebe6612d40e0a867c4a4e981e
Simon Liang <siliang@deloitte.co.nz>
2019-02-16 13:49:01 +1300

    Solution for Problem 1: Two Sum

---
+++ src/Algorithms/Problem_1_Two_Sum.java
@@ -0,0 +1,18 @@
+package Algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+class Problem_1_Two_Sum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> value_map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            Integer value = value_map.get(target - nums[i]);
+            if (null != value)
+                return new int[] {value , i};
+            value_map.put(nums[i], i);
+        }
+        return null;
+    }
+}
