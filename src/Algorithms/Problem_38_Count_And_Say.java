1d7debf35da5f146bc3474d4594b2296f5ca7427
liangxibing <simonlaung@gmail.com>
2019-10-20 11:40:33 +1300

    Improve Solution for Problem 38: Count and Say

--- src/Algorithms/Problem_38_Count_And_Say.java
+++ src/Algorithms/Problem_38_Count_And_Say.java
@@ -7,19 +7,19 @@ public class Problem_38_Count_And_Say {
         char[] prev = countAndSay(n - 1).toCharArray();
         int i = 0;
         int count = 1;
-        String output = "";
+        StringBuilder output = new StringBuilder();
         while (i < prev.length - 1) {
             if (prev[i] == prev[i + 1]) {
                 count++;
                 i++;
                 continue;
             }
-            output += "" + count + prev[i];
+            output.append(count).append(prev[i]);
             count = 1;
             i++;
         }
-        output += "" + count + prev[i];
+        output.append(count).append(prev[i]);
 
-        return output;
+        return output.toString();
     }
 }
